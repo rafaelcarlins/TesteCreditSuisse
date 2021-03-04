4bb7a8f40fad61f770b46bf42df3a610928a3aaf
Rafael <Rafael@LAPTOP-SV57APN8>
2021-03-04 13:42:55 -0300

    tabela de input de dados

---
+++ Tabela e procedures/InputTableCategory.sql
@@ -0,0 +1,11 @@
+﻿insert into Category(Value, ValueGreaterThan,ClientSector, Active, Risk)
+values('1000000', 0 ,'Public', 1,'LOWRISK')
+
+insert into Category(Value, ValueGreaterThan,ClientSector, Active, Risk)
+values('1000000', 1 ,'Public', 1,'MEDIUMRISK')
+
+insert into Category(Value, ValueGreaterThan,ClientSector, Active, Risk)
+values('1000000', 1 ,'Private', 1,'HIGHRISK')
+
+--select * from Category
+--drop table Category
